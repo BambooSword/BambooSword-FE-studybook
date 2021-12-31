a1076bd5120f53bf583e236b1704bf0582ac5d0f
BambooSword <herodreamdeng@gmail.com>
2021-12-31 18:14:08 +0800

    doc: add the articles link

--- __tests__/mock_new/index.spec.js
+++ __tests__/mock_new/index.spec.js
@@ -1,28 +1,36 @@
 import { mockNew } from '../../code/interview/mock_new';
 function returnObj() {
+  this.type = 'object';
   return {
     return: 'obj',
   };
 }
 function returnNumber() {
+  this.type = 'number';
   return 0;
 }
 function returnBoolean() {
+  this.type = 'boolean';
   return false;
 }
 function returnSymbol() {
+  this.type = 'symbol';
   return Symbol('hello');
 }
 function returnString() {
+  this.type = 'string';
   return 'abc';
 }
 function returnUndefined() {
+  this.type = 'undefined';
   return undefined;
 }
 function returnNull() {
+  this.type = 'null';
   return null;
 }
 function returnBigInt() {
+  this.type = 'BigInt';
   return 999n;
 }
 describe('test mockNew', () => {
@@ -32,24 +40,38 @@ describe('test mockNew', () => {
     });
   });
   test('return Number', () => {
-    expect(mockNew(returnNumber)).toEqual({});
+    expect(mockNew(returnNumber)).toEqual({
+      type: 'number',
+    });
   });
   test('return Boolean', () => {
-    expect(mockNew(returnBoolean)).toEqual({});
+    expect(mockNew(returnBoolean)).toEqual({
+      type: 'boolean',
+    });
   });
   test('return Symbol', () => {
-    expect(mockNew(returnSymbol)).toEqual({});
+    expect(mockNew(returnSymbol)).toEqual({
+      type: 'symbol',
+    });
   });
   test('return String', () => {
-    expect(mockNew(returnString)).toEqual({});
+    expect(mockNew(returnString)).toEqual({
+      type: 'string',
+    });
   });
-  test('return String', () => {
-    expect(mockNew(returnUndefined)).toEqual({});
+  test('return Undefined', () => {
+    expect(mockNew(returnUndefined)).toEqual({
+      type: 'undefined',
+    });
   });
   test('return Null', () => {
-    expect(mockNew(returnNull)).toEqual({});
+    expect(mockNew(returnNull)).toEqual({
+      type: 'null',
+    });
   });
   test('return BigInt', () => {
-    expect(mockNew(returnBigInt)).toEqual({});
+    expect(mockNew(returnBigInt)).toEqual({
+      type: 'BigInt',
+    });
   });
 });
--- code/interview/mock_new.js
+++ code/interview/mock_new.js
@@ -5,3 +5,39 @@ export function mockNew() {
   const res = constructor.call(obj, arguments);
   return typeof res !== 'object' || res === null ? obj : res;
 }
+/**
+ * 这是一个模拟 new 操作符的函数
+ * @param {constructor} fn 构造函数
+ * @returns
+ */
+function NewFn(fn) {
+  // 1. 创建一个新的空对象
+  const obj = {};
+  // 2. **设置构造函数的原型**
+  // 2. 设置空对象的原型，让其指向为构造函数的prototype属性
+  obj.__proto__ = fn.prototype;
+  // 3. 让构造函数的this指向新的空对象，并执行构造函数
+  const res = fn.call(obj);
+  // 4. 判断构造函数的返回类型，如果是值类型，则返回 obj, 如果是引用类型，则返回该引用类型的对象
+  // return (typeof res ===  'object' && res !== null) ? res : obj;
+  const isYinYong = typeof res === 'object' && res !== null;
+  if (isYinYong) {
+    return res;
+  } else {
+    return obj;
+  }
+}
+
+function A() {
+  this.a = 'this is a ';
+  return 1;
+}
+function B() {
+  this.b = 'this si b ';
+  return {
+    b: 'dddddddd',
+  };
+}
+console.log(new A());
+console.log(new B());
+console.log('模拟 new A 的函数 NewFn(A)', NewFn(A));
