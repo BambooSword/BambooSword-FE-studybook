88a729729598dfc75bedea66b4562cbd79524b2a
BambooSword <herodreamdeng@gmail.com>
2022-01-10 16:46:08 +0800

    create or update file + 1

--- test.js
+++ test.js
@@ -2,10 +2,328 @@
 
 
 
-sss
-ss
+### 2021.11.03
+
+第二周第2套算法题来啦~限时两天！
+[好的]话不多说开始吧！
+
+#### [剑指 Offer II 024. 反转链表](https://leetcode-cn.com/problems/UHnkqh/) (done)
+
+#### [面试题 02.07. 链表相交](https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/) (done)
+
+#### [61. 旋转链表](https://leetcode-cn.com/problems/rotate-list/)(done)
+
+#### [703. 数据流中的第 K 大元素](https://leetcode-cn.com/problems/kth-largest-element-in-a-stream/)(done)
+
+#### [226. 翻转二叉树](https://leetcode-cn.com/problems/invert-binary-tree/)(done)
+
+这次咱们直接LeetCode刷啦
 
 
-ssss
-sszdds
-sss
+
+### 2021.11.05
+
+[嘿哈]周末弯道超车~冲就完事！
+
+本周的第三套题提前给到咱们啦，前两套做完就开刷吧
+周一记得分享你的产出哦[OK]
+#### [110. 平衡二叉树](https://leetcode-cn.com/problems/balanced-binary-tree/)(done)
+
+#### [剑指 Offer 55 - I. 二叉树的深度](https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/)[done]
+
+#### [35. 搜索插入位置](https://leetcode-cn.com/problems/search-insert-position/)(done)
+
+#### [13. 罗马数字转整数](https://leetcode-cn.com/problems/roman-to-integer/)(done)
+
+#### [344. 反转字符串](https://leetcode-cn.com/problems/reverse-string/)（done）
+
+
+
+### 2021.11.09
+
+
+
+新的一周[旺柴]上一周刷的怎么样？有收获没？
+
+本周第一套题可以开始刷啦[OK]
+#### [剑指 Offer 06. 从尾到头打印链表](https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/)(done)
+
+#### [面试题 02.02. 返回倒数第 k 个节点](https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci/)
+
+#### [141. 环形链表](https://leetcode-cn.com/problems/linked-list-cycle/)
+
+#### [25. K 个一组翻转链表](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/)
+
+#### [剑指 Offer II 004. 只出现一次的数字 ](https://leetcode-cn.com/problems/WGki4K/)
+
+
+
+### 2021.11.10
+
+第六期算法题：
+
+#### [633. 两数平方数之和](https://leetcode-cn.com/problems/sum-of-square-numbers/)
+
+#### [912. 二路归并排序](https://leetcode-cn.com/problems/sort-an-array/)
+
+#### [704. 二分查找](https://leetcode-cn.com/problems/binary-search/)
+
+#### [1. 哈希_两数之和](https://leetcode-cn.com/problems/two-sum/)
+
+#### [ 最长递增子序列](https://leetcode-cn.com/problems/longest-increasing-subsequence/) => [二分查找思路](https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/dong-tai-gui-hua-he-er-fen-cha-zhao-lian-x7dh/)
+
+
+
+### 2021.11.12
+
+第七期算法题：
+桶排序：https://leetcode-cn.com/problems/top-k-frequent-elements/description/
+快乐数：https://leetcode-cn.com/problems/happy-number/
+两节点的最长路径：https://leetcode.com/problems/diameter-of-binary-tree/description/
+两个字符串包含的字符是否完全相同：https://leetcode-cn.com/problems/valid-anagram/description/
+计算一组字符集合可以组成的回文字符串的最大长度：https://leetcode-cn.com/problems/longest-palindrome/description/
+
+
+
+### 2021.11.15
+
+第八套算法，他来了~  ：
+
+#### [剑指 Offer II 095. 最长公共子序列](https://leetcode-cn.com/problems/qJnOS7/)
+
+#### [121. 买卖股票的最佳时机](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/)
+
+#### [746. 使用最小花费爬楼梯](https://leetcode-cn.com/problems/min-cost-climbing-stairs/)
+
+#### [226. 翻转二叉树](https://leetcode-cn.com/problems/invert-binary-tree/)
+
+#### [647. 回文子串](https://leetcode-cn.com/problems/palindromic-substrings/)
+
+
+
+
+
+
+
+### 2021.11.17
+
+gogogo！第九次算法题：
+
+
+#### [剑指 Offer 22. 链表中倒数第k个节点](https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/)
+
+#### [138. 复制带随机指针的链表](https://leetcode-cn.com/problems/copy-list-with-random-pointer/)
+
+#### [1721. 交换链表中的节点](https://leetcode-cn.com/problems/swapping-nodes-in-a-linked-list/)
+
+#### [24. 两两交换链表中的节点](https://leetcode-cn.com/problems/swap-nodes-in-pairs/)
+
+#### [23. 合并K个升序链表](https://leetcode-cn.com/problems/merge-k-sorted-lists/)
+
+
+
+
+
+### 2021.11.22
+
+久等啦，第十套算法题，开肝！
+#### [145. 二叉树的后序遍历](https://leetcode-cn.com/problems/binary-tree-postorder-traversal/)
+
+> **[102. 二叉树的层序遍历](https://leetcode-cn.com/problems/binary-tree-level-order-traversal/)**
+
+#### [331. 验证二叉树的前序序列化](https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/)
+
+#### [基本计算器 II]( https://leetcode-cn.com/problems/basic-calculator-ii )
+
+> [224. 基本计算器](https://leetcode-cn.com/problems/basic-calculator/)[227. 基本计算器 II](https://leetcode-cn.com/problems/basic-calculator-ii/)[基本计算器 III](https://leetcode-cn.com/problems/basic-calculator-iii/)[770. 基本计算器 IV](https://leetcode-cn.com/problems/basic-calculator-iv/)
+
+#### 函数的独占时间 https://leetcode-cn.com/problems/exclusive-time-of-functions 
+#### 表现良好的最长时间段 https://leetcode-cn.com/problems/longest-well-performing-interval 
+
+
+
+### 2021.11.24
+
+第十一套算法题，坚持！
+环形链表 II https://leetcode-cn.com/problems/linked-list-cycle-ii 
+反转链表 https://leetcode-cn.com/problems/reverse-linked-list 
+反转链表 II https://leetcode-cn.com/problems/reverse-linked-list-ii 
+删除链表的倒数第 N 个结点 https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list 
+删除排序链表中的重复元素 https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list
+删除排序链表中的重复元素 II https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii  
+
+
+
+### 2021.11.26
+
+周五来活啦！
+五道算法，LeetCode开整
+1、分隔链表        https://leetcode-cn.com/problems/partition-list
+2、 设计循环队列        https://leetcode-cn.com/problems/design-circular-queue
+3、设计循环双端队列        https://leetcode-cn.com/problems/design-circular-deque
+4、设计前中后队列        https://leetcode-cn.com/problems/design-front-middle-back-queue
+5、最近的请求次数        https://leetcode-cn.com/problems/number-of-recent-calls
+
+
+
+### 2021.11.29
+
+
+
+前方高能！！！第十三套算法题来袭！！！
+1、[面试题 17.09. 第 k 个数](https://leetcode-cn.com/problems/get-kth-magic-number-lcci/)                 [剑指 Offer 49. 丑数](https://leetcode-cn.com/problems/chou-shu-lcof/)
+2、 [859. 亲密字符串](https://leetcode-cn.com/problems/buddy-strings/)
+3、 [860. 柠檬水找零](https://leetcode-cn.com/problems/lemonade-change/)
+4、 [969. 煎饼排序](https://leetcode-cn.com/problems/pancake-sorting/)
+5、 [621. 任务调度器](https://leetcode-cn.com/problems/task-scheduler/)
+
+### 2021.12.01
+
+第十四套算法来袭，请各位继续努力~
+1、[面试题 03.04. 化栈为队](https://leetcode-cn.com/problems/implement-queue-using-stacks-lcci/)
+2、[682. 棒球比赛](https://leetcode-cn.com/problems/baseball-game/)
+3、[844. 比较含退格的字符串](https://leetcode-cn.com/problems/backspace-string-compare/)
+4、[946. 验证栈序列](https://leetcode-cn.com/problems/validate-stack-sequences/)
+5、[1021. 删除最外层的括号](https://leetcode-cn.com/problems/remove-outermost-parentheses/)
+
+### 2021.12.03
+
+第十五套算法题，明天又是六日啦，一定要劳逸结合哦😋
+1、剑指 Offer ！复杂链表的复制        https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof
+2、 删除中间节点        https://leetcode-cn.com/problems/delete-middle-node-lcci
+3、两数相加 II        https://leetcode-cn.com/problems/add-two-numbers-ii
+4、 重排链表        https://leetcode-cn.com/problems/reorder-list
+5、.移除无效的括号        https://leetcode-cn.com/problems/minimum-remove-to-make-valid-parentheses   
+
+### 2021.12.06
+
+第十六套算法题🔥
+1、[面试题 02.08. 环路检测](https://leetcode-cn.com/problems/linked-list-cycle-lcci/)
+2、设计链表 https://leetcode-cn.com/problems/design-linked-list
+3、剑指 Offer： 删除链表的节点 https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+4、分隔链表 https://leetcode-cn.com/problems/split-linked-list-in-parts
+5、面试题：分割链表 https://leetcode-cn.com/problems/partition-list-lcci
+
+
+
+### 2021.12.08
+
+🤔第17套算法题
+1、Offer 10： 斐波那契数列 https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
+2、路径总和：https://leetcode-cn.com/problems/path-sum
+3、从前序与中序遍历序列构造二叉树 https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+4、 第K个语法符号 https://leetcode-cn.com/problems/k-th-symbol-in-grammar
+5、 完全二叉树的节点个数 https://leetcode-cn.com/problems/count-complete-tree-nodes
+
+### 2021.12.13
+
+周一周一，算法飞起！
+1、N 叉树的前序遍历： https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal 
+2、翻转二叉树 ：https://leetcode-cn.com/problems/invert-binary-tree 
+3、剑指 Offer：从上到下打印二叉数II https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof 
+4、二叉树的层序遍历 II： https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii 
+5、 二叉树的锯齿形层序遍历： https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal 
+
+
+
+### 2021.12.15
+
+本周第二套题，咱们现在第一阶段要保证50到算法题产出以上，所以，大家，勤奋，加油！
+1、 平衡二叉树 和 2021.11.05重复
+2、 [剑指 Offer 40. 最小的k个数  LCOF](https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/)
+3、 最后一块石头的重量 https://leetcode-cn.com/problems/last-stone-weight 
+4、 数据流中的第 K 大元素 和 2021.11.03 重复
+5、 查找和最小的K对数字 https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums
+
+
+
+### 2021.12.20
+
+周一先来点轻松的，本套题难度适中~~
+祝顺利完成！！
+
+1、 [1801. 积压订单中的订单总数](https://leetcode-cn.com/problems/number-of-orders-in-the-backlog/)
+2、[264. 丑数 II](https://leetcode-cn.com/problems/ugly-number-ii/)
+3、[313. 超级丑数](https://leetcode-cn.com/problems/super-ugly-number/)
+4、[1753. Maximum Score From Removing Stones](https://leetcode-cn.com/problems/maximum-score-from-removing-stones/)
+5、 [104. 二叉树的最大深度](https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/)[done, 和2021.11.05 2相同]
+
+
+
+### 2021.12.22
+
+本周第二套算法题来啦，是他是他就是他.....
+1.[面试题 04.05. 合法二叉搜索树](https://leetcode-cn.com/problems/legal-binary-search-tree-lcci/)
+
+2.[230. 二叉搜索树中第K小的元素](https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/)
+3.[199. 二叉树的右视图](https://leetcode-cn.com/problems/binary-tree-right-side-view/)
+4.[100. 相同的树](https://leetcode-cn.com/problems/same-tree/)
+5.[101.对称二叉树 Symmetric Tree](https://leetcode-cn.com/problems/symmetric-tree/)
+
+### 2021.12.24
+
+本周第三套，研究起来
+1、[235. 二叉搜索树的最近公共祖先](https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
+2、 [124. 二叉树中的最大路径和](https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/)
+3、[347. 前 K 个高频元素](https://leetcode-cn.com/problems/top-k-frequent-elements/)
+4、 [973. 最接近原点的 K 个点](https://leetcode-cn.com/problems/k-closest-points-to-origin/)
+5、根据字符出现频率排序 https://leetcode-cn.com/problems/sort-characters-by-frequency（next time）
+
+
+
+### 2021.12.27
+
+本周第一套算法题~
+1、 [547. 省份数量](https://leetcode-cn.com/problems/number-of-provinces/)
+2、https://leetcode-cn.com/problems/number-of-islands/
+3、 等式方程的可满足性 https://leetcode-cn.com/problems/satisfiability-of-equality-equations 
+4、冗余连接 https://leetcode-cn.com/problems/redundant-connection 
+5、 连通网络的操作次数 https://leetcode-cn.com/problems/number-of-operations-to-make-network-connected 
+
+
+
+### 2021.12.29
+
+本周第二套算法题
+128. 最长连续序列  https://leetcode-cn.com/problems/longest-consecutive-sequence
+947. 移除最多的同行或同列石头  https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column
+1202. 交换字符串中的元素  https://leetcode-cn.com/problems/smallest-string-with-swaps
+721. 账户合并  https://leetcode-cn.com/problems/accounts-merge
+765. 情侣牵手  https://leetcode-cn.com/problems/couples-holding-hands
+
+
+
+### 2021.12.31
+
+本周第三套算法题
+685. 冗余连接 II https://leetcode-cn.com/problems/redundant-connection-ii 
+239. 滑动窗口最大值 https://leetcode-cn.com/problems/sliding-window-maximum 
+470. 用 Rand7() 实现 Rand10() https://leetcode-cn.com/problems/implement-rand10-using-rand7 
+11. 盛最多水的容器 https://leetcode-cn.com/problems/container-with-most-water 
+394. 字符串解码 https://leetcode-cn.com/problems/decode-string 
+
+元旦回来，我们开始第二阶段内容上传喽
+
+
+
+### 2022.01.03
+
+本周第一套题来啦
+1.394. 字符串解码 https://leetcode-cn.com/problems/decode-string 
+2.95. 不同的二叉搜索树 II https://leetcode-cn.com/problems/unique-binary-search-trees-ii 
+3.75. 颜色分类 https://leetcode-cn.com/problems/sort-colors 
+4.面试题 17.14. 最小K个数 https://leetcode-cn.com/problems/smallest-k-lcci 
+5.剑指 Offer 21. 调整数组顺序使奇数位于偶数前面 https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof 
+
+
+
+### 2022.01.07
+
+本周第二套题哦
+1、 排序数组 https://leetcode-cn.com/problems/sort-an-array 
+2、剑指 Offer 51. 数组中的逆序对 https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof 
+3、 排序链表 https://leetcode-cn.com/problems/sort-list 
+4、 两棵二叉搜索树中的所有元素 https://leetcode-cn.com/problems/all-elements-in-two-binary-search-trees 
+5、面试题 04.08. 首个共同祖先 https://leetcode-cn.com/problems/first-common-ancestor-lcci 
+
+
